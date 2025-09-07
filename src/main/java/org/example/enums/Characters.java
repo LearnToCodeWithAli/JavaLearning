c14dfa582a94afd63022343d0c07358f1e0e35aa
LearnToCodeWithAli <learn.to.code.with.ali@gmail.com>
2025-09-07 10:03:28 -0700

    removing constructor from the enum

--- src/main/java/org/example/enums/Characters.java
+++ src/main/java/org/example/enums/Characters.java
@@ -1,19 +1,10 @@
 package org.example.enums;
 
 public enum Characters {
-    KATARA("Katara"),
-    AANG("Aang"),
-    SOKKA("Sokka"),
-    MOMO("Momo"),
-    APPA("Appa"),
-    TOPH("Toph");
-
-    private String name;
-
-    private Characters(String name) {
-        this.name = name;
-    }
-    public String getName() {
-        return name;
-    }
+    KATARA,
+    AANG,
+    SOKKA,
+    MOMO,
+    APPA,
+    TOPH;
 }
